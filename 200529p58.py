da6cc8464ffb2c60807a8ba8afceefdeda61dd80
chyko67 <66109346+chyko67@users.noreply.github.com>
2020-05-30 13:34:54 +0900

    Add files via upload

---
+++ 200529p58.py
@@ -0,0 +1,8 @@
+import re
+
+first_text = input('Enter the first text: ')
+second_text = input('Enter the second text: ')
+
+
+print(first_text[len(first_text)-1] == second_text[len(second_text)-1])
+    
